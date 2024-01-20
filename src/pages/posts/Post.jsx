9e1e19131468aa428ee8d576947c5e9be923832b
Haniibani <hanna.behmer@gmail.com>
2024-01-20 17:05:39 +0000

    Fix post page functionality

--- src/pages/posts/Post.jsx
+++ src/pages/posts/Post.jsx
@@ -1,13 +1,93 @@
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
 
-const Post = () => {
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../styles/App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../clients/axios";
+
+import Post from "../../components/Post";
+import Comment from "../../components/Comment";
+
+import CommentCreate from "../comments/CommentCreate";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import fetchMoreData from "../../utils/fetchMoreData";
+import PopularProfiles from "../../components/PopularProfiles";
+
+function PostPage() {
   const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
+      } catch (err) {
+        // console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
-    <div>
-      <h1>{`Hello from post:${id}`}</h1>
-    </div>
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreate
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
   );
-};
+}
 
-export default Post
+export default PostPage;
