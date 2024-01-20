12eaed06e012875dbcdbdc06dba516faa1390c80
Haniibani <hanna.behmer@gmail.com>
2024-01-20 17:51:27 +0000

    Fix loading bootstrap styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
+
 import App from "./App";
 
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import { ProfileDataProvider } from "./contexts/ProfileDataContext";
+
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
