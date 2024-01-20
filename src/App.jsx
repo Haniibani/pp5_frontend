150798f0bbc3a9fc2530a9772cea346188a79384
Haniibani <hanna.behmer@gmail.com>
2024-01-20 11:30:28 +0000

    Add auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+
 import { Container } from "react-bootstrap";
 import { Switch, Route } from "react-router-dom";
+
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
 
 const App = () => {
   return (
     <div className="App">
       <Container>
         <Switch>
-          <Route path="/" render={() => <h1>Hello world</h1>} />
+          <Route exact path="/" render={() => <h1>Hello world</h1>} />
+          <Route exact path="/signin" render={() => <SignIn />} />
+          <Route exact path="/signup" render={() => <SignUp />} />
         </Switch>
       </Container>
     </div>
