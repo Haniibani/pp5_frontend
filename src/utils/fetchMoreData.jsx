7fd4bae45f4f5234c01904b73586fcae0adf45f6
Haniibani <hanna.behmer@gmail.com>
2024-01-20 13:06:13 +0000

    Add fetch more data helper function

---
+++ src/utils/fetchMoreData.jsx
@@ -0,0 +1,22 @@
+const fetchMoreData = async (resource, setResource) => {
+  try {
+    // Fetch data from the 'next' URL
+    const { data } = await axiosReq.get(resource.next);
+
+    // Update the 'resource' state
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        // Check if the current result already exists in 'results'
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur]; // Add the current result to 'results' if it's unique
+      }, prevResource.results),
+    }));
+  } catch (err) {
+    // Handle errors if necessary
+  }
+};
+
+export default fetchMoreData;
