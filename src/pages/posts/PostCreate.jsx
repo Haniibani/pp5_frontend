0923f42ba3aa142fd917ab15eba5a00c8e3d90ee
Haniibani <hanna.behmer@gmail.com>
2024-01-21 07:44:46 +0000

    Add select many fields for tags when creating new post

--- src/pages/posts/PostCreate.jsx
+++ src/pages/posts/PostCreate.jsx
@@ -20,44 +20,48 @@ import { axiosReq } from "../../clients/axios";
 
 import useRedirect from "../../hooks/useRedirect";
 
+import tags from "../../constants/tags";
+
 const PostCreate = () => {
   useRedirect("loggedOut");
   const [errors, setErrors] = useState({});
-
   const [postData, setPostData] = useState({
     title: "",
     content: "",
     image: "",
+    tags: [],
   });
-  const { title, content, image } = postData;
-
+  const { title, content, image, tags: selectedTags } = postData;
   const imageInput = useRef(null);
   const history = useHistory();
 
-  const handleChange = (event) => {
-    setPostData({
-      ...postData,
-      [event.target.name]: event.target.value,
-    });
+  const handleChange = ({ target: { name, value } }) =>
+    setPostData((prevState) => ({ ...prevState, [name]: value }));
+
+  const handleTagsChange = ({ target: { options } }) => {
+    const selectedOptions = Array.from(options)
+      .filter((option) => option.selected)
+      .map((option) => parseInt(option.value));
+    setPostData((prevState) => ({ ...prevState, tags: selectedOptions }));
   };
 
-  const handleChangeImage = (event) => {
-    if (event.target.files.length) {
+  const handleChangeImage = ({ target: { files } }) => {
+    if (files.length) {
       URL.revokeObjectURL(image);
-      setPostData({
-        ...postData,
-        image: URL.createObjectURL(event.target.files[0]),
-      });
+      setPostData((prevState) => ({
+        ...prevState,
+        image: URL.createObjectURL(files[0]),
+      }));
     }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-
     formData.append("title", title);
     formData.append("content", content);
     formData.append("image", imageInput.current.files[0]);
+    formData.append("tags", selectedTags.join(","));
 
     try {
       const { data } = await axiosReq.post("/posts/", formData);
@@ -69,100 +73,91 @@ const PostCreate = () => {
     }
   };
 
-  const textFields = (
-    <div className="text-center">
-      <Form.Group>
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          type="text"
-          name="title"
-          value={title}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      {errors?.title?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-
-      <Form.Group>
-        <Form.Label>Content</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={6}
-          name="content"
-          value={content}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      {errors?.content?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-
-      <Button
-        variant="outline-primary"
-        className="btn btn-block"
-        onClick={() => history.goBack()}
-      >
-        Cancel
-      </Button>
-      <Button variant="primary" type="submit" className="btn btn-block">
-        Create
-      </Button>
-    </div>
-  );
+  const renderAlerts = (field) =>
+    errors[field]?.map((message, idx) => (
+      <Alert variant="warning" key={idx}>
+        {message}
+      </Alert>
+    ));
 
   return (
     <Form onSubmit={handleSubmit}>
       <Row>
-        <Col md={7} lg={8}>
-          <Container className="d-flex flex-column justify-content-center">
+        <Col md={6}>
+          <Container className="d-flex flex-column justify-content-center align-items-center">
             <Form.Group className="text-center">
-              {image ? (
-                <>
-                  <figure>
-                    <Image src={image} rounded />
-                  </figure>
-                  <div>
-                    <Form.Label
-                      htmlFor="image-upload"
-                      className="btn btn-outline-primary"
-                    >
-                      Change the image
-                    </Form.Label>
-                  </div>
-                </>
-              ) : (
-                <Form.Label
-                  htmlFor="image-upload"
-                  className="d-flex justify-content-center"
-                >
-                  <Asset
-                    src={Upload}
-                    message="Click or tap to upload an image"
-                  />
-                </Form.Label>
-              )}
-
+              <label
+                htmlFor="image-upload"
+                className={`btn ${
+                  image ? "btn-outline-primary" : "btn-primary"
+                }`}
+              >
+                {image ? (
+                  <Image src={image} rounded />
+                ) : (
+                  <Asset src={Upload} message="Upload an image" />
+                )}
+              </label>
               <Form.File
                 id="image-upload"
                 accept="image/*"
                 onChange={handleChangeImage}
                 ref={imageInput}
+                hidden
               />
+              {renderAlerts("image")}
             </Form.Group>
-            {errors?.image?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
           </Container>
         </Col>
-        <Col md={5} lg={4} className="d-none d-md-block">
-          <Container>{textFields}</Container>
+        <Col md={6}>
+          <Container className="text-center">
+            <Form.Group>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleChange}
+              />
+              {renderAlerts("title")}
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Content</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={6}
+                name="content"
+                value={content}
+                onChange={handleChange}
+              />
+              {renderAlerts("content")}
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Tags</Form.Label>
+              <Form.Control
+                as="select"
+                multiple
+                value={selectedTags}
+                onChange={handleTagsChange}
+              >
+                {tags.map((tag) => (
+                  <option key={tag.id} value={tag.id}>
+                    {tag.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Button
+              variant="outline-primary"
+              onClick={() => history.goBack()}
+              className="btn-block"
+            >
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit" className="btn-block">
+              Create
+            </Button>
+          </Container>
         </Col>
       </Row>
     </Form>
