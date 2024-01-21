ac43ccfea7f13c728ca524be006d623e82dd7a79
Hanna Behmer <122392326+Haniibani@users.noreply.github.com>
2024-01-21 21:13:57 +0100

    Update design on signin page

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -20,6 +20,11 @@ import useRedirect from "../../hooks/useRedirect";
 
 import setTokenTimestamp from "../../utils/setTokenTimestamp"
 
+import Technology from '../../assets/Technology.png';
+import Travel from '../../assets/Travel.png';
+import Art from '../../assets/Art.png';
+import History from '../../assets/History.png';
+
 const SignIn = () => {
   const setCurrentUser = useSetCurrentUser();
   useRedirect("loggedIn");
@@ -54,71 +59,85 @@ const SignIn = () => {
   };
 
   return (
-    <Row>
-      <Col md={6}>
-        <Container className="p-4">
-          <h1 className="text-center">Sign In</h1>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label className="visually-hidden">Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors?.username?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
+    <Container>
+      <Row className="justify-content-center">
+        <Col md={8}>
+          <Container className="p-4">
+            <h1 className="text-center">Sign In</h1>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="username">
+                <Form.Label className="visually-hidden">Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors?.username?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                  {message}
+                </Alert>
+              ))}
 
-            <Form.Group controlId="password">
-              <Form.Label className="visually-hidden">Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors?.password?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
+              <Form.Group controlId="password">
+                <Form.Label className="visually-hidden">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors?.password?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                  {message}
+                </Alert>
+              ))}
 
-            <Button
-              type="submit"
-              variant="primary"
-              className="btn btn-block"
-              style={{ cursor: "pointer" }}
-            >
-              Sign In
-            </Button>
-            {errors?.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
-                {message}
-              </Alert>
-            ))}
-          </Form>
-        </Container>
+              <Button
+                type="submit"
+                variant="success"
+                className="btn btn-block"
+                style={{ cursor: "pointer" }}
+              >
+                Sign In
+              </Button>
+              {errors?.non_field_errors?.map((message, idx) => (
+                <Alert key={idx} variant="warning" className="mt-3">
+                  {message}
+                </Alert>
+              ))}
+            </Form>
+          </Container>
 
-        <Container className="mt-3">
-          <Link to="/signup" className="text-decoration-none">
-            Don't have an account? Sign up now!
-          </Link>
-        </Container>
-      </Col>
-      <Col md={6} className="d-none d-md-block p-2">
-        <Image
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
-        />
-      </Col>
-    </Row>
+          <Container className="text-center mt-3">
+            <span>
+              Don't have an account?{' '}
+              <Link to="/signup" className="text-decoration-none" style={{ fontWeight: 'bold', color: '#28a745' }}>
+                Sign up now!
+              </Link>{' '}
+            </span>
+          </Container>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mb-4">
+        <Col xs={6} md={3} className="p-2">
+          <Image src={Technology} fluid />
+        </Col>
+        <Col xs={6} md={3} className="p-2">
+          <Image src={Travel} fluid />
+        </Col>
+        <Col xs={6} md={3} className="p-2">
+          <Image src={Art} fluid />
+        </Col>
+        <Col xs={6} md={3} className="p-2">
+          <Image src={History} fluid />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
