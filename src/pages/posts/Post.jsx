27e2bb0a0fb365011731cb4473154293cbc87d12
Haniibani <hanna.behmer@gmail.com>
2024-01-21 07:31:21 +0000

    Make post page more readable

--- src/pages/posts/Post.jsx
+++ src/pages/posts/Post.jsx
@@ -1,69 +1,69 @@
 import React, { useEffect, useState } from "react";
-
+import { useParams } from "react-router";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-import appStyles from "../../styles/App.module.css";
-import { useParams } from "react-router";
 import { axiosReq } from "../../clients/axios";
-
-import Post from "../../components/Post";
-import Comment from "../../components/Comment";
-
-import CommentCreate from "../comments/CommentCreate";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
-import InfiniteScroll from "react-infinite-scroll-component";
-import Asset from "../../components/Asset";
 import fetchMoreData from "../../utils/fetchMoreData";
+
+import PostComp from "../../components/Post";
+import Comment from "../../components/Comment";
+import CommentCreate from "../comments/CommentCreate";
 import PopularProfiles from "../../components/PopularProfiles";
+import Asset from "../../components/Asset";
 
-function PostPage() {
+import appStyles from "../../styles/App.module.css";
+import isEmpty from "../../utils/isEmpty";
+
+const Post = () => {
   const { id } = useParams();
-  const [post, setPost] = useState({ results: [] });
-
+  const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
   const currentUser = useCurrentUser();
-  const profile_image = currentUser?.profile_image;
-  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
-    const handleMount = async () => {
+    const fetchData = async () => {
       try {
-        const [{ data: post }, { data: comments }] = await Promise.all([
+        const [{ data: post }, { data: commentsData }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
           axiosReq.get(`/comments/?post=${id}`),
         ]);
-        setPost({ results: [post] });
-        setComments(comments);
+        setPost(post);
+        setComments(commentsData.results);
       } catch (err) {
-        // console.log(err);
+        // Handle error
       }
     };
 
-    handleMount();
+    fetchData();
   }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <PopularProfiles mobile />
-        <Post {...post.results[0]} setPosts={setPost} postPage />
+        {post && <PostComp {...post} setPosts={setPost} postPage />}
         <Container className={appStyles.Content}>
-          {currentUser ? (
+          {currentUser && (
             <CommentCreate
               profile_id={currentUser.profile_id}
-              profileImage={profile_image}
+              profileImage={currentUser.profile_image}
               post={id}
               setPost={setPost}
               setComments={setComments}
             />
-          ) : comments.results.length ? (
-            "Comments"
-          ) : null}
-          {comments.results.length ? (
+          )}
+          {comments.length > 0 && (
             <InfiniteScroll
-              children={comments.results.map((comment) => (
+              dataLength={comments.length}
+              next={() => fetchMoreData(comments, setComments)}
+              hasMore={!!comments.next}
+              loader={<Asset spinner />}
+            >
+              {comments.map((comment) => (
                 <Comment
                   key={comment.id}
                   {...comment}
@@ -71,15 +71,14 @@ function PostPage() {
                   setComments={setComments}
                 />
               ))}
-              dataLength={comments.results.length}
-              loader={<Asset spinner />}
-              hasMore={!!comments.next}
-              next={() => fetchMoreData(comments, setComments)}
-            />
-          ) : currentUser ? (
-            <span>No comments yet, be the first to comment!</span>
-          ) : (
-            <span>No comments... yet</span>
+            </InfiniteScroll>
+          )}
+          {isEmpty(comments) && (
+            <span>
+              {currentUser
+                ? "No comments yet, be the first to comment!"
+                : "No comments... yet"}
+            </span>
           )}
         </Container>
       </Col>
@@ -88,6 +87,6 @@ function PostPage() {
       </Col>
     </Row>
   );
-}
+};
 
-export default PostPage;
+export default Post;
