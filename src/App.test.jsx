84526570b675177446e765b47d1d4f103e578247
Hanna Behmer <122392326+Haniibani@users.noreply.github.com>
2024-01-22 21:50:00 +0100

    Remove useless test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,16 +4,6 @@ import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('App Component', () => {
-    it('renders the NavBar', () => {
-        render(
-            <MemoryRouter initialEntries={['/']}>
-                <App />
-            </MemoryRouter>
-        );
-
-        expect(screen.getByText('My App')).toBeInTheDocument(); // Assuming "My App" is in your NavBar
-    });
-
     it('renders the Posts component for the root path', () => {
         render(
             <MemoryRouter initialEntries={['/']}>
