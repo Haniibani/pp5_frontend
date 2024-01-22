29d616498e0f67de95759a87119b081e9d33269e
Hanna Behmer <122392326+Haniibani@users.noreply.github.com>
2024-01-22 17:39:30 +0100

    Add filter on a post tag

--- src/components/FilterFeed.jsx
+++ src/components/FilterFeed.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useCallback } from 'react';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
-import appStyles from "../styles/App.module.css";
+
 import { Container, Row, Col } from 'react-bootstrap';
+
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+
+import appStyles from "../styles/App.module.css";
+
+import tags from '../constants/tags';
 
 const FilterFeed = ({ setFilter, mobile }) => {
     const currentUser = useCurrentUser();
@@ -9,6 +14,7 @@ const FilterFeed = ({ setFilter, mobile }) => {
 
     const [isCheckedLikes, setIsCheckedLikes] = useState(false);
     const [isCheckedFollowed, setIsCheckedFollowed] = useState(false);
+    const [selectedTag, setSelectedTag] = useState('');
 
     const handleLikesChange = useCallback(() => {
         setIsCheckedLikes(prev => {
@@ -19,7 +25,12 @@ const FilterFeed = ({ setFilter, mobile }) => {
         if (isCheckedFollowed) {
             setIsCheckedFollowed(false);
         }
-    }, [isCheckedFollowed, setFilter, profile_id]);
+
+        if (selectedTag !== '') {
+            setSelectedTag('');
+        }
+
+    }, [isCheckedFollowed, setFilter, profile_id, selectedTag]);
 
     const handleFollowedChange = useCallback(() => {
         setIsCheckedFollowed(prev => {
@@ -30,7 +41,30 @@ const FilterFeed = ({ setFilter, mobile }) => {
         if (isCheckedLikes) {
             setIsCheckedLikes(false);
         }
-    }, [isCheckedLikes, setFilter, profile_id]);
+
+        if (selectedTag !== '') {
+            setSelectedTag('');
+        }
+
+    }, [isCheckedLikes, setFilter, profile_id, selectedTag]);
+
+    const handleTagChange = useCallback((e) => {
+        const selectedValue = e.target.value;
+        setSelectedTag(selectedValue);
+        setFilter(`tag=${selectedValue}`)
+
+        if (isCheckedFollowed) {
+            setIsCheckedFollowed(false);
+        }
+
+        if (isCheckedLikes) {
+            setIsCheckedLikes(false);
+        }
+
+
+    }, [isCheckedFollowed, isCheckedLikes]);
+
+    if (!currentUser) return null;
 
     return (
         <Container className={`${appStyles.Content} mb-2 ${mobile ? "d-lg-none text-center my-3" : ""}`}>
@@ -51,6 +85,23 @@ const FilterFeed = ({ setFilter, mobile }) => {
                         checked={isCheckedFollowed}
                         onChange={handleFollowedChange}
                     /> Show posts from Followed Accounts
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <label htmlFor="tagSelect">Filter on Tag: </label>
+                    <select
+                        id="tagSelect"
+                        value={selectedTag}
+                        onChange={handleTagChange}
+                    >
+                        <option value="">Select a Tag</option>
+                        {tags.map((tag) => (
+                            <option key={tag.id} value={tag.id}>
+                                {tag.name}
+                            </option>
+                        ))}
+                    </select>
                 </Col>
             </Row>
         </Container>
