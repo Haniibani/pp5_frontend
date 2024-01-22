89b1f9656ca53577695201746b5fd3e37211bcf1
Hanna Behmer <122392326+Haniibani@users.noreply.github.com>
2024-01-22 17:39:58 +0100

    Use correct no results icon

--- src/components/ProfilePosts.jsx
+++ src/components/ProfilePosts.jsx
@@ -3,7 +3,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../components/Post";
 import Asset from "../components/Asset";
 import fetchMoreData from "../utils/fetchMoreData";
-import NoResults from "../assets/upload-icon.png";
+import NoResults from "../assets/NotFound.png";
 
 const ProfilePosts = ({ profilePosts, setProfilePosts }) => (
   <>
